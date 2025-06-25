9a24dc60d0e9bf946e1aeb3d0ac9f890977aea5d
SuryaPandian2005 <141639035+SuryaPandian2005@users.noreply.github.com>
2025-06-25 20:34:46 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,15 +13,17 @@ export const Home = () => {
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
       {/* Theme Toggle */}
       <ThemeToggle />
+
       {/* Background Effects */}
       <StarBackground />
 
       {/* Navbar */}
       <Navbar />
+
       {/* Main Content */}
-      <main>
+      <main className="pt-24">
         <HeroSection />
-        
+
         <AboutSection />
         <SkillsSection />
         <ProjectsSection />
