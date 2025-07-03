ebb95624ba697d2dbeca973c821c90878388fd8e
SuryaPandian2005 <141639035+SuryaPandian2005@users.noreply.github.com>
2025-07-03 14:45:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
+import  RockPaperScissors  from "./pages/RockPaperScissors";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/rps" element={<RockPaperScissors />} />
         </Routes>
       </BrowserRouter>
     </>
